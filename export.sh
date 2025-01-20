e7dc570d76d5c2b682efc52f6f846552e695f937
yangb <yangb05@pcl.ac.cn>
2025-01-20 15:35:52 +0800

    fix some bugs

--- export.sh
+++ export.sh
@@ -1,5 +1,5 @@
 # export pretrained.pt for finetune
 ./zipformer/export.py \
-  --epoch 43 \
-  --avg 15 \
-  --config-file config_run/example.yaml
+  --epoch 75 \
+  --avg 11 \
+  --config-file config_train/example.yaml
--- export_onnx.sh
+++ export_onnx.sh
@@ -1,5 +1,5 @@
 # export onnx for deployment
 python zipformer/export-onnx-streaming.py \
-  --epoch 43 \
-  --avg 15 \
+  --epoch 75 \
+  --avg 11 \
   --config-file config_train/example.yaml
